51228299ecd303c2417dfff4ecdc0cb878c3d48c
Luuwwo <119816199+Luuwwo@users.noreply.github.com>
2025-04-11 18:50:09 +0300

    Updates

---
+++ precache-manifest.e8565ab5bc6a162954b3d91979490836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cdd03ac451120308efa5d1c968a916",
+    "url": "/fefem.git/index.html"
+  },
+  {
+    "revision": "44f39499fdb34ffab328",
+    "url": "/fefem.git/static/css/main.b29a6fa6.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/fefem.git/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "44f39499fdb34ffab328",
+    "url": "/fefem.git/static/js/main.2e8e44c0.chunk.js"
+  },
+  {
+    "revision": "742a96c646372c4375c1",
+    "url": "/fefem.git/static/js/runtime~main.e6eb6581.js"
+  },
+  {
+    "revision": "aae7427ebfbe2a393b94f6b3528b7df6",
+    "url": "/fefem.git/static/media/logo.aae7427e.png"
+  }
+]);
